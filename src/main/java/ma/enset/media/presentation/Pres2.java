79cb813694d3153b3087e00fa85aa82637906486
moussafia <moussafia.mohammed1999@gmail.com>
2025-03-16 11:41:19 +0000

    instatntaion dynamique

---
+++ src/main/java/ma/enset/media/presentation/Pres2.java
@@ -0,0 +1,26 @@
+package ma.enset.media.presentation;
+
+import ma.enset.media.dao.IDao;
+import ma.enset.media.metier.Imetier;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Scanner;
+
+public class Pres2 {
+    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        System.out.println("Instanciation dynamique");
+        Scanner scanner = new Scanner(new File("src/config.txt"));
+        String daoClassName = scanner.nextLine();
+        Class cdao = Class.forName(daoClassName);
+        IDao d = (IDao) cdao.newInstance();
+
+        String metierClassName = scanner.nextLine();
+        Class cMetier = Class.forName(metierClassName);
+        Imetier imetier = (Imetier) cMetier.getConstructor(IDao.class).newInstance(d);
+
+        System.out.println("Resultat : " + imetier.calcule());
+
+    }
+}
